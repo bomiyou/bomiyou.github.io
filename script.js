2172b212dc772a3c26e45526dd40265acc28bde0
Bomi You <bomi7054@gmail.com>
2021-02-01 16:28:53 -0500

    Update script.js
    Updated AJAX mechanism for it to be dynamic.

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ function loadpage(e){
   $("#menu a.active").removeClass("active");
   $(this).addClass("active");
 
-  $("#content").load("about.html");
+  var href = $(this).attr("href");
+  $("#content").load(href);
   
 }
 
